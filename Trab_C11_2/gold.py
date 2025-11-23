8d6449b147cd21f6c22ad5496bf9c83b3924536a
Henrique Issao Kato <114307623+HenriqueIK@users.noreply.github.com>
2025-11-23 13:07:43 -0300

    Arima arrumado
    Coloquei as duas previsoes para 10 anos e arrumei o arima

--- Trab_C11_2/gold.py
+++ Trab_C11_2/gold.py
@@ -33,20 +33,21 @@ df['China(CNY)']['31-01-2000':].plot(figsize=(8,6))
 predictions.plot()
 plt.show()
 
-model2 = auto_arima(y = df['China(CNY)'], m =12)
+model2 = auto_arima(df['China(CNY)'], m=12, seasonal=True, trace=True)
 
 freq = pd.infer_freq(df.index)
 if freq is None:
     freq = 'M'
-    
+
 start = df.index[-1] + pd.tseries.frequencies.to_offset(freq)
 
-#coloquei 36 meses para testar mas nao ta indo aaaaaaaaaaaaaaaaaaaaaaaaaaa
-pred_index = pd.date_range(start=start, periods = 36, freq=freq)
+#prevendo 10 anos (120 meses)
+pred_index = pd.date_range(start = start, periods = 120, freq = freq)
 
-predictions2 = pd.Series(arima_values, freq = freq)
+arima_values = model2.predict(n_periods = 120)
+
+predictions2 = pd.Series(arima_values, index = pred_index)
 
 df['China(CNY)']['31-01-2000':].plot(figsize=(8,6))
-
 predictions2.plot()
 plt.show()
